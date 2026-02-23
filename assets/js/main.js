439ee50272f6fcaca159779cd4c6680122390548
Camilamparrag <camilaparragon@gmail.com>
2026-02-23 15:43:23 +0000

    js ejemplos

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+//let: define variables con alcance de bloque y valor reasignable.
+//const: define constantes de solo 
+
+
+// let le dice al interprete que esto es una variable
+let nombre = "Camila"
+ 
+let saludo = "hola"
+
+// esto es una variable constante, no cambia
+const pi = 3.141516
+
+
+//este console.log manda al navegador lo que le pasemos
+console.log(saludo)
+
+
+//esto es texto
+let texto = "esto es un string"
+
+// tipo de dato number
+let numeros = 100
+
+// tipo de dato booleano
+// representa si es verdadero o falso
+let verdadero = true
+let falso = false
+
+let esMayor = true
+
+// concatenacion
+//siempre va a tener representación con los textos
+
+let nombre1 = "Camila"
+let nombre2 = "Monserrat"
+let edad = 33
+
+console.log("Hola me llamo "  + nombre1 + " " + nombre2 + " " + "y tengo " + edad  + "años");
+
+
+//operadores matematicos
+let a = 1
+let b = 2
+
+//suma
+console.log(a + b);
+//resta
+console.log(a - b);
+//multiplacion
+console.log(a * b);
+//division
+console.log(a / b);
+//modulo
+console.log(a % b);
+ 
+
+let pepito = document.getElementById("h1");
+
+pepito.textContent = "holaaaa"
+pepito.style.color = "green"
+pepito.style.fontSize = "400px"
+
+
